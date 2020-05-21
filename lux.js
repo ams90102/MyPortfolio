4581c0af27ba2438e7d1c3ca909a9ecd3c085660
ams90102 <ams901026@gmail.com>
2020-05-22 02:56:01 +0800

    lux-project:mobile微修正

--- lux.js
+++ lux.js
@@ -1,7 +1,26 @@
-// $(window).scroll(function(){
-//     var scroll = $(window).scrollTop(),
-//         dh = $(document).height(),
-//         wh = $(window).height();
-//         value = (scroll/(dh-wh)) * 100;
-//     $('#container').css('left', -value * 3 + '%')
-// })
+$(window).scroll(function(){
+    if (window.matchMedia("(max-width: 900px)").matches) {
+        /* 窗口小于或等于 700 像素 */
+        $('#container').css('left', 0 + '%')
+    } else {
+        /*窗口大于 700 像素 */
+        var scroll = $(window).scrollTop(),
+            dh = $(document).height(),
+            wh = $(window).height();
+            ww = $(window).width();
+            value = (scroll / (dh-ww)) *100;
+        $('#container').css('left', - value * (dh/ww) + '%');
+
+        //     value = (scroll/(dh-wh)) * 100;
+        // $('#container').css('left', -value * 3 + '%')
+        
+        //     if (window.matchMedia("(max-width: 1024px)").matches){
+        //         value = (scroll/ww) * 50;
+        //     }
+        //     else{
+        //         value = (scroll/ww) * 60;
+        //     }
+        //     // value = (scroll/(dh-wh)) * 100;
+        // $('#container').css('left', -value * 3 + '%')
+    }
+});
