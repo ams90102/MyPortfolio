3bcc02f1479739b9ae869dcd6ac8d58142821860
ams90102 <ams901026@gmail.com>
2020-05-23 13:16:38 +0800

    lux-project:測試手機

--- lux.js
+++ lux.js
@@ -1,22 +1,22 @@
 $(window).scroll(function(){
-    if (window.matchMedia("(max-width: 900px)").matches) {
-        /* 窗口小于或等于 700 像素 */
+    if (window.matchMedia("(max-width: 1000px)").matches) {
+        /* 窗口小于或等于 1000 像素 */
         $('#container').css('left', 0 + '%')
     } else {
-        /*窗口大于 700 像素 */
+        /*窗口大于 1000 像素 */
         var scroll = $(window).scrollTop(),
             dh = $(document).height(),
             wh = $(window).height();
             ww = $(window).width();
-        //     value = (scroll/(dh-wh)) * 100;
-        // $('#container').css('left', -value * 3 + '%')
-        value = scroll/(dh-wh) *100;
-        // 4是頁數
-        if(ww<1150){
-            $('#container').css('left', - value *(dh/ww) + '%');
-        }
-        else{
-            $('#container').css('left', - value * 3 + '%');
-        }
+            value = (scroll/(dh-wh)) * 100;
+        $('#container').css('left', -value * 3 + '%')
+        // value = scroll/(dh-wh) *100;
+        // // 4是頁數
+        // if(ww<1150){
+        //     $('#container').css('left', - value *(dh/ww) + '%');
+        // }
+        // else{
+        //     $('#container').css('left', - value * 3 + '%');
+        // }
     }
 });
